3436ac6e93261ba26b7aaa4661ad49ba174b09ba
cknckz <8832791+cknckz@users.noreply.github.com>
2024-08-01 16:48:27 +0200

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World CKN!\n');
+  res.send('Hello CKN welcome!\n');
 });
 
 app.listen(8080, function () {
